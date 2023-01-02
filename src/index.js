b6d0949520225f86a03d2fef98fb7cdd77633bc9
charlie-s1 <charlie.d.smith98@gmail.com>
2023-01-02 13:03:28 +0000

    add past work

---
+++ src/index.js
@@ -0,0 +1,216 @@
+import React from 'react';
+import {createRoot} from 'react-dom/client';
+import { TypeAnimation } from 'react-type-animation';
+import "./index.css";
+
+
+import mainFace from "./img/main.png";
+import eye from "./img/lEye.png";
+import closeEye from "./img/eyeBlink.png";
+import pupil from "./img/pupil.png";
+import smile from "./img/smile.png";
+import eyeBack from "./img/eyeBackground.png";
+// import smSmile from "./img/smSmile.png";
+import talk from "./img/talk.gif";
+import relaxEye from "./img/relaxEye.png"
+import closeSmile from "./img/closeSmile.png"
+
+let images = [
+  mainFace,
+  eye,
+  closeEye,
+  pupil,
+  smile,
+  eyeBack,
+  talk,
+  relaxEye,
+  closeSmile
+]
+
+function mountImages(){
+  const toRet = images.map((i)=>{
+    const img = new Image();
+    img.src = i;
+    img.draggable = false;
+    return img;
+  });
+  return toRet;
+}
+
+class Face extends React.Component{
+  constructor(props){
+    super(props);
+    this.lEyeRef = this.props.lEyeRef;
+    this.rEyeRef = this.props.rEyeRef;
+    this.faceRef = this.props.faceRef;
+    this.state={
+      hover:false,
+      lEyeHover:false,
+      rEyeHover:false,
+    }
+
+    this.images = mountImages();
+    console.log(this.images);
+  }
+ 
+  render(){
+    return(
+      <div id="FaceCont"
+        onLoad={(e)=>this.props.onLoad(e)}
+      >
+        <div className="link" 
+          onMouseEnter={(e)=>{this.state.hover=true}}
+          onMouseLeave={(e)=>{this.state.hover=false}}
+        >
+          <h1>PORTFOLIO</h1>
+        </div>
+        <div id="face" ref={this.faceRef}>
+          <img draggable={false} id="mainFace" src={mainFace}/>
+          <div id="faceParts">
+            <div id="lEyeCont" ref={this.lEyeRef}>
+              <div id="lEye"
+                onMouseEnter={()=>{this.state.lEyeHover=true}}
+                onMouseLeave={()=>{this.state.lEyeHover=false}}
+              >
+                <img draggable={false} id="leftEyeBack" src={eyeBack}></img>
+                <div style={{transform:`rotate(${this.props.lRot}deg)`}} className="eye" id="lPupilCont">
+                  <img style={{left:`${this.props.pupilPosX}%`,top:`${this.props.pupilPosY}%`}} draggable={false} id="lPupil" src={pupil}/>
+                </div>
+                <img 
+                  draggable={false} 
+                  id="leftEyeImg" 
+                  style={this.props.hovering ? (this.state.lEyeHover ? {height:"150%"} : null) : {left:"-15%",maxWidth:"105%"}} 
+                  src={this.props.hovering ? (this.state.lEyeHover ? closeEye : eye) : relaxEye}
+                />
+
+              </div>
+            </div>
+            <div id="rEyeCont" ref={this.rEyeRef}>
+              <div id="rEye"
+                onMouseEnter={()=>{this.state.rEyeHover=true}}
+                onMouseLeave={()=>{this.state.rEyeHover=false}}
+              >
+                <img draggable={false} id="rightEyeBack" src={eyeBack}></img>
+                <div  style={{transform:`rotate(${this.props.rRot}deg)`}} className="eye" id="rPupilCont">
+                  <img style={{left:`${this.props.pupilPosX}%`,top:`${this.props.pupilPosY}%`}} draggable={false} id="rPupil" src={pupil}/>
+                </div>
+                <img 
+                  draggable={false} 
+                  id="rightEyeImg"
+                  style={this.props.hovering ? (this.state.rEyeHover ? {height:"150%"} : null) : {left:"10%",maxWidth:"105%"}} 
+                  src={this.props.hovering ? (this.state.rEyeHover ? closeEye : eye) : relaxEye}
+                />
+              </div>
+            </div>
+            <div id="mouthCont">
+              <img draggable={false} id="mouth" src={this.props.hovering ? (this.state.hover ? smile :this.props.talking ? talk : closeSmile) : this.props.talking ? talk : smile}></img>
+            </div>
+            
+            
+            {/* <div id="smile" style={{opacity:`${this.state.hover ? 1:0}`}}>
+              <img src={smile}/>
+            </div> */}
+          </div>
+          
+        </div>
+        <div className="link" 
+          onMouseEnter={(e)=>{this.state.hover=true}}
+          onMouseLeave={(e)=>{this.state.hover=false}}
+        >
+          <h1>PAST WORK</h1>
+        </div>
+        
+      </div>
+    )
+  }
+}
+
+
+class Main extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+      talking:false,
+      showing:"",
+      lRot:0,
+      rRot:0,
+      pupilPosX:50,
+      pupilPosY:50,
+      hovering:false,
+      loaded:false,
+    }
+    this.lEyeRef = React.createRef();
+    this.rEyeRef = React.createRef();
+    this.faceRef = React.createRef();
+    this.talking();
+  }
+  /**
+   * move eye pupil 
+   */
+  mouseMoving(e){
+    let lDifX = (this.faceRef.current.offsetLeft + (this.lEyeRef.current.offsetLeft + this.lEyeRef.current.offsetWidth/2)) - e.pageX;
+    let lDifY = (this.faceRef.current.offsetTop + (this.lEyeRef.current.offsetTop + this.lEyeRef.current.offsetHeight/2)) - e.pageY;
+    let rDifX = (this.faceRef.current.offsetLeft + (this.rEyeRef.current.offsetLeft + this.rEyeRef.current.offsetWidth/2)) - e.pageX;
+    let rDifY = (this.faceRef.current.offsetTop + (this.rEyeRef.current.offsetTop + this.rEyeRef.current.offsetHeight/2)) - e.pageY;
+
+
+    let lAngle = (Math.atan2(lDifX,lDifY)*180)/Math.PI;
+    let rAngle = (Math.atan2(rDifX,rDifY)*180)/Math.PI;
+    this.setState({
+      lRot:-lAngle,
+      rRot:-rAngle,
+    })
+  }
+  /**
+   * stop mouth moving when done talking
+   */
+  talking(){
+    setTimeout(()=>{
+      this.setState({talking:true})
+    },1000);
+    setTimeout(()=>{
+      this.setState({talking:false})
+    },3500);
+  }
+
+  render(){
+    return(
+      <div id="main"
+        
+        onMouseMove={(e)=>this.mouseMoving(e)}
+        onMouseLeave={(e)=>{this.setState({hovering:false,pupilPosX:50,pupilPosY:50})}}
+        onMouseEnter={(e)=>{this.setState({hovering:true,pupilPosX:50,pupilPosY:25})}}
+        style={this.state.loaded ? {display:"flex"} : {display:"none"}}
+      >
+        <Face
+          onLoad={(e)=>this.setState({loaded:true})}
+          talking={this.state.talking}
+          lRot={this.state.lRot}
+          rRot={this.state.rRot}
+          pupilPosX={this.state.pupilPosX}
+          pupilPosY={this.state.pupilPosY}
+          rEyeRef = {this.rEyeRef}
+          lEyeRef = {this.lEyeRef}
+          faceRef = {this.faceRef}
+          hovering = {this.state.hovering}
+        />
+        <TypeAnimation
+          sequence={[
+            1000,
+            "Hi, my name is Alannah. Have a look around!",
+          ]}
+          wrapper="div"
+          className="speech"
+          cursor={false}
+          speed={50}
+        />
+      </div>
+    )
+  }
+}
+
+const container = document.querySelector("#root");
+const root = createRoot(container);
+root.render(
+  <Main/>
+)
